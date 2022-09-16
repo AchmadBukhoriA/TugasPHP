2f720ce37e33a1ae50ec9bf4e80a2497a2b55117
AchmadBukhoriA <112109464+AchmadBukhoriA@users.noreply.github.com>
2022-09-16 09:55:05 +0700

    .

---
+++ loginAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN ADMIN</title>
+</head>
+<body>
+    <h1>DATA SISWA(ADMIN)</h1>
+    <table border="1" cellpadding="10" cellspacing="0" width ="100%">
+    <tr>
+        <th>No</th>
+        <th>Foto</th>
+        <th>Nama</th>
+        <th>Aksi</th>
+    </tr>
+    <tr>
+        <th>1</th>
+        <th><img src="foto.jpg.png" width="20%"></th>
+        <th>Bambang</th>
+        <th><button>Detail</button></th>
+    </tr>
+    <tr>
+        <th>2</th>
+        <th><img src="foto.jpg.png" width="20%"></th>
+        <th>Jojo</th>
+        <th><button>Detail</button></th>
+    </tr>
+</body>
+</html>
---
+++ loginSiswa.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN SISWA</title>
+</head>
+<body>
+    <h1>DATA SISWA</h1>
+    <table border="1" width ="100%">
+    <tr>
+        <th>No</th>
+        <th>Foto</th>
+        <th>Nama</th>
+    </tr>
+    <tr>
+        <th>1</th>
+        <th><img src="foto.jpg.png" width="20%"></th>
+        <th>Bambang</th>
+    </tr>
+    <tr>
+        <th>2</th>
+        <th><img src="foto.jpg.png" width="20%"></th>
+        <th>Jojo</th>
+    </tr>
+    </table>
+</body>
+</html>
